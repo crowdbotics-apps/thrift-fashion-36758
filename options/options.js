4f822d8fed497a3cf2af97b41bfc59734f745b35
Crowdbotics <team@crowdbotics.com>
2022-08-13 00:38:01 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "thrift_fashion_36758",
+  url: "https://thrift_fashion_36758.botics.co",
+  api: "https://thrift_fashion_36758.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
